2877efb0550cb728fc67a0f4ad05e94cacc8fdf2
Evan-Hoffman <evanhoffman370@gmail.com>
2020-09-15 18:49:45 -0400

    setting up server

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<hi>Home Page</h1>")
+})
+
+app.listen(6000, () => {
+    console.log("Server started on Port 6000");
+})
