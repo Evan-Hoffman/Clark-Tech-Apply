96c75680eddf4c1c9cd9e2f75f0669814e5a1cd6
Evan-Hoffman <evanhoffman370@gmail.com>
2020-09-15 19:23:39 -0400

    added db info

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require("express");
-const app = express();
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config({path: './.env'});
 
 
+//setup Database connection:
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DATABASE
+});
+
+db.connect( (error) => {
+    if(error){
+        console.log(error)
+    }
+    else {
+        console.log("MYSQL Connected")
+    }
+})
 
 app.get("/", (req, res) => {
     res.send("<hi>Home Page</h1>")
