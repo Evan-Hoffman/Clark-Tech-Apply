4301482c6a599d83c513c6ebd9d1d62713a7f5c2
Evan-Hoffman <evanhoffman370@gmail.com>
2020-09-16 02:19:51 -0400

    added error catching in registration form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,33 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DATABASE
+});
+
 exports.register = (req, res) => {
     console.log(req.body);
 
     const {name, email, password, passwordConfirm} = req.body;
 
+    db.query('SELECT email FROM users WHERE email = ?', [email], (error, results)=>{
+        if (error) {
+            console.log(error);
+        }
+        if (results.length > 0){
+            return res.render('register', {
+                message: 'That email is already in use'
+            })
+        }   else if (password != passwordConfirm) {
+                return res.render('register', {
+                    message: 'Passwords do not match'
+                });
+            
+        }
+    })
+
+    
     res.send("Form submitted");
 }
