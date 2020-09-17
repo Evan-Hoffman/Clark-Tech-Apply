cec801c10eb3db420963ba65fce632d0af841cb7
Evan-Hoffman <evanhoffman370@gmail.com>
2020-09-17 01:27:21 -0400

    made nav bar pages dependable on if you are logged in or not

--- routes/pages.js
+++ routes/pages.js
@@ -3,8 +3,10 @@ const authController = require('../controllers/auth')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', authController.isLoggedIn, (req, res) => {
+    res.render('index', {
+        user: req.user
+    });
 });
 
 router.get('/register', (req, res) => {
@@ -18,7 +20,9 @@ router.get('/login', (req, res) => {
 //only go to profile if logged in
 router.get('/profile', authController.isLoggedIn, (req, res) => {
     if(req.user) {
-        res.render('profile');
+        res.render('profile', {
+            user: req.user
+        });
     }
     else {
         res.redirect('/login');
