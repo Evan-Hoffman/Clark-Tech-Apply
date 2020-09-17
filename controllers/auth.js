066de0600bb52db5bee7b175f20dd5da7be76f12
Evan-Hoffman <evanhoffman370@gmail.com>
2020-09-17 01:35:51 -0400

    added logout functionality and destruction of cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -121,3 +121,11 @@ exports.isLoggedIn = async (req, res, next) => {
         next();
     }
 }
+
+exports.logout = async (req, res) => {
+    res.cookie('jtoken', 'logout', {
+        expires: new Date(Date.now() + 2*1000),
+        httpOnly: true
+    });
+    res.status(200).redirect('/');
+}
--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,6 @@ router.post('/register', authController.register);
 
 router.post('/login', authController.login);
 
+router.get('/logout', authController.logout);
+
 module.exports = router;
